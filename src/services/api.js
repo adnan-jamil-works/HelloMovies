c269a533d95f77f7be9813419bf711149ba5e925
adnanjamil77 <Adnanjamil.intern@devsinc.com>
2025-09-25 15:30:17 -0700

    intialTest1

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "72d8a3a15de958c4179aeb4328b9d44b";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
